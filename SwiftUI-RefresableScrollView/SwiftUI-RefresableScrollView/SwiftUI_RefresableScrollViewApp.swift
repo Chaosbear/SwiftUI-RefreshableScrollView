f1ef07f872ff4abb2442be5d7eb32695f9126e16
krit sukrit <sukrit@dek-d.com>
2023-06-18 14:41:20 +0700

    - init xcode project

---
+++ SwiftUI-RefresableScrollView/SwiftUI-RefresableScrollView/SwiftUI_RefresableScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_RefresableScrollViewApp.swift
+//  SwiftUI-RefresableScrollView
+//
+//  Created by Sukrit Chatmeeboon on 18/6/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_RefresableScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
